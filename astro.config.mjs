00127b38015ed9a9e2033b7e4f446e680b557b97
n1ghthouse <121154936+n1ghthouse@users.noreply.github.com>
2024-11-27 18:23:30 +0500

    🩹 Testing Clarity script as a JS as well

--- astro.config.mjs
+++ astro.config.mjs
@@ -60,6 +60,17 @@ export default defineConfig({
             },
             {
                 tag: 'script',
+                type: 'text/javascript',
+                children: `
+                    (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                    })(window, document, "clarity", "script", "ke9gk0s2sg");
+                `,
+            },
+            {
+                tag: 'script',
                 type: 'text/partytown',
                 children: `
                     (function(c,l,a,r,i,t,y){
