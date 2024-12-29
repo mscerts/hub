a7b97f2b52e74f76ffcd28d9d02698600695016a
trueberryless <trueberryless+github@gmail.com>
2024-12-29 22:52:51 +0100

    format content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,5 +4,10 @@ import { docsLoader } from "@astrojs/starlight/loaders";
 import { blogSchema } from 'starlight-blog/schema';
 
 export const collections = {
-  docs: defineCollection({ loader: docsLoader(), schema: docsSchema({ extend: (context) => blogSchema(context) }) }),
+  docs: defineCollection({
+    loader: docsLoader(),
+    schema: docsSchema({
+      extend: (context) => blogSchema(context),
+    }),
+  }),
 };
