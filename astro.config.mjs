136665d315a4c6c661b4b4f2ead1d81aad8e9a6a
teriaavibes [MVP, MCT Regional Lead] <68708075+teriaavibes@users.noreply.github.com>
2025-01-16 15:10:02 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -153,7 +153,7 @@ export default defineConfig({
                     { label: 'Mindhub Replay Voucher Bundles', link: '/vouchers/mindhubreplayvoucherbundles/' },						
                     { label: 'Power Up Program', link: '/vouchers/powerupprogram/' },
                     { label: 'Virtual Training Days', link: '/vouchers/virtualtrainingdays/' },
-		    { label: 'DP-600 for Microsoft Partners', link: '/vouchers/DP-600 for Microsoft Partners/'},
+		    { label: 'DP-600 for Microsoft Partners', link: '/vouchers/dp-600formicrosoftpartners.mdx/'},
                 ],
             },
             {
