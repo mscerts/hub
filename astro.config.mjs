fa1b860bcde9023b21e3c5ac57967daa0bce8f67
teriaavibes [MVP, MCT Regional Lead] <68708075+teriaavibes@users.noreply.github.com>
2025-01-16 21:25:35 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -147,14 +147,14 @@ export default defineConfig({
             {
                 label: 'Voucher Offers',
                 items: [
-                    { label: 'Beta Exams', link: '/vouchers/betaexams/' },
-                    { label: 'Cloud Skills Challenges', link: '/vouchers/cloudskillschallenges/',badge: { text: 'N/A', variant: 'danger' }, },
-                    { label: 'Microsoft ESI', link: '/vouchers/microsoftesi/' ,badge: { text: '50%', variant: 'note' }, },
-                    { label: 'Mindhub Replay Voucher Bundles', link: '/vouchers/mindhubreplayvoucherbundles/' },						
-                    { label: 'Power Up Program', link: '/vouchers/powerupprogram/' ,badge: { text: '100%', variant: 'note' }, },
-                    { label: 'Virtual Training Days', link: '/vouchers/virtualtrainingdays/', badge: { text: '50%', variant: 'note' }, },
-		    { label: 'DP-600 for Partners', link: '/vouchers/dp-600forpartners/' ,badge: { text: '100%', variant: 'note' }, },
 		    { label: 'DP-700 Discount', link: '/vouchers/dp-700discount/' ,badge: { text: '50%', variant: 'note' }, }
+		    { label: 'DP-600 for Partners', link: '/vouchers/dp-600forpartners/' ,badge: { text: '100%', variant: 'note' }, },
+                    { label: 'Virtual Training Days', link: '/vouchers/virtualtrainingdays/', badge: { text: '50%', variant: 'note' }, },
+                    { label: 'Microsoft ESI', link: '/vouchers/microsoftesi/' ,badge: { text: '50%', variant: 'note' }, },
+                    { label: 'Power Up Program', link: '/vouchers/powerupprogram/' ,badge: { text: '100%', variant: 'note' }, },
+		    { label: 'Mindhub Replay Voucher Bundles', link: '/vouchers/mindhubreplayvoucherbundles/' },						
+                    { label: 'Beta Exams', link: '/vouchers/betaexams/' },
+
                 ],
             },
             {
