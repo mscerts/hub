245dbd293421dfe4bdec846801763e070bc0831d
teriaavibes [MVP, MCT Community Lead] <68708075+teriaavibes@users.noreply.github.com>
2025-02-13 19:28:00 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -152,7 +152,7 @@ export default defineConfig({
                     { label: 'Virtual Training Days', link: '/vouchers/virtualtrainingdays/' ,badge: { text: '50%', variant: 'note' }, },
                     { label: 'Microsoft ESI', link: '/vouchers/microsoftesi/' ,badge: { text: '50%', variant: 'note' }, },
                     { label: 'Power Up Program', link: '/vouchers/powerupprogram/' ,badge: { text: '100%', variant: 'note' }, },						
-                    { label: 'Beta Exams', link: '/vouchers/betaexams/' ,badge: { text: '80%+25%', variant: 'note' }, },
+                    { label: 'Beta Exams', link: '/vouchers/betaexams/' ,badge: { text: '80% + 25%', variant: 'note' }, },
 		    { label: 'Replay/Retake Vouchers', link: '/vouchers/mindhubreplayvoucherbundles/' },
 
                 ],
