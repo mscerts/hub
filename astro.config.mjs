876df352934abf1c412be1d4a4dae0131625ba3a
teriaavibes [MVP, MCT Community Lead] <68708075+teriaavibes@users.noreply.github.com>
2025-10-03 15:44:14 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -161,6 +161,7 @@ export default defineConfig({
         {
             label: 'Discounted Exam Vouchers',
             items: [
+				{ label: 'Microsoft Partner LevelUp', link: '/vouchers/levelup/' ,badge: { text: '*100%', variant: 'tip' }, },
                 { label: 'FabCon Vienna Offer', link: '/vouchers/fabricviennaoffer/' ,badge: { text: '50%', variant: 'tip' }, },
                 { label: 'Applied Skills Sweepstakes', link: '/vouchers/appliedskillssweepstakes/' ,badge: { text: '*50%', variant: 'tip' }, },
                 { label: 'Virtual Training Days', link: '/vouchers/virtualtrainingdays/' ,badge: { text: '50%', variant: 'note' }, },
