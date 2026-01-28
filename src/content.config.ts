682801f7cabc5fe277a0f7e3114c69e4dc5bf74e
n1ghthouse <121154936+n1ghthouse@users.noreply.github.com>
2026-01-28 16:00:17 +0500

    adds notice banner of migration

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,4 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
 import { docsLoader } from "@astrojs/starlight/loaders";
 import { blogSchema } from 'starlight-blog/schema';
@@ -7,6 +7,17 @@ import { videosSchema } from 'starlight-videos/schemas'
 export const collections = {
   docs: defineCollection({
     loader: docsLoader(),
-    schema: docsSchema({ extend: (context) => videosSchema.merge(blogSchema(context)) }),
+    schema: docsSchema({ 
+      extend: (context) => videosSchema.merge(blogSchema(context)).merge(
+        z.object({
+          // Add a default value to the built-in `banner` field.
+          banner: z.object({ content: z.string() }).default({
+            content: `<a href="https://msfthub.com" target="_blank">
+  We are migrating to a new domain! Click here to visit MSFTHub.com.
+</a>`,
+          }),
+        })
+      )
+    }),
   }),
 };
